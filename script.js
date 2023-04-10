87d685d8269c31c54b5e51cbd05a2fdd703dc72e
Fatemeh Mohammadi <ftmmohammadi1805@gmail.com>
2023-04-10 23:23:06 +0430

    animation loading

---
+++ script.js
@@ -0,0 +1,47 @@
+const elementL = document.querySelector("#div1");
+
+
+elementL.addEventListener('animationend', () => {
+  elementL.style.color = "#e7f20c";
+console.log("red")
+});
+const elementO = document.querySelector("#div2");
+
+elementO.addEventListener('animationend', () => {
+  elementO.style.color = "#0003c7";
+console.log("red")
+});
+const elementA = document.querySelector("#div3");
+
+elementA.addEventListener('animationend', () => {
+    elementA.style.color = "#cd0402";
+console.log("red")
+});
+const elementD = document.querySelector("#div4");
+
+elementD.addEventListener('animationend', () => {
+    elementD.style.color = "#d23600";
+console.log("red")
+});
+const elementI = document.querySelector("#div5");
+
+elementI.addEventListener('animationend', () => {
+    elementI.style.color = "#8c06e6";
+console.log("red")
+});
+const elementN = document.querySelector("#div6");
+
+elementN.addEventListener('animationend', () => {
+    elementN.style.color = "#f30ac8";
+console.log("red")
+});
+const elementG = document.querySelector("#div7");
+
+elementG.addEventListener('animationend', () => {
+    elementG.style.color = "#aad011";
+console.log("red")
+});
+
+
+
+
